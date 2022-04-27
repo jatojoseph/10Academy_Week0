b4bbd59454791bd6b8860b10cab7b41cf0c372dd
jatojoseph <jaotjoseph4@gmail.com>
2022-04-27 11:53:12 +0100

    unit test works

--- tests/test_extract_dataframe.py
+++ tests/test_extract_dataframe.py
@@ -8,7 +8,7 @@ sys.path.append(os.path.abspath(os.path.join('../..')))
 
 #print (path)
 #'/home/jay/env_twitter/10Academy0/10Academy_Week0/home/jay/env_twitter/10Academy0/10Academy_Week0/'
-from extract_dataframe.py import read_json
+from extract_dataframe import read_json
 from extract_dataframe import TweetDfExtractor
 
 
